57722e88a537173b5111d2a94cdbffc3ca977979
Fernando Barros <FernandoBarros@compta.pt>
2022-03-06 12:52:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+def submit():
+    print("A Temperatura é de: "+str(scale.get())+ " graus C")
+
+window = Tk()
+#window.geometry("420x420")
+window.title("A minha primeira Janela Python")
+
+icon = PhotoImage(file='estrela.png')
+window.iconphoto(True,icon)
+window.config(bg="white")
+
+scale =Scale(window,
+             from_=100,
+             to=0,
+             length=600,
+             orient=VERTICAL,
+             font=('Consolas',20),
+             tickinterval=10,
+             showvalue=0,
+             resolution=2)
+scale.pack()
+
+button=Button(window,text="submit",command=submit)
+button.pack()
+
+window.mainloop()
